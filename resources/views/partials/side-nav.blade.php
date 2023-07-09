0d6e451803a100385195a0da3de354f7f930351d
Arun Kumar <arunmaurya2500@gmail.com>
2023-07-10 02:40:57 +0530

    active url synchronized  with active link

--- resources/views/partials/side-nav.blade.php
+++ resources/views/partials/side-nav.blade.php
@@ -28,7 +28,8 @@
                 </li>
                 <!--[ Start:: dashboard ]-->
                 <li class="collapsed">
-                    <a class="m-link active" data-bs-toggle="collapse" data-bs-target="#menu_dashboard" href="#">
+                    <a class="m-link  {{ request()->is('admin/dashboard') ? 'active' : '' }}" data-bs-toggle="collapse"
+                        data-bs-target="#menu_dashboard" href="#">
                         <svg width="20" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
                             <path opacity="0.2"
                                 d="M8.186 1.11303C8.06662 1.06519 7.93338 1.06519 7.814 1.11303L1.846 3.50003L8 5.96103L14.154 3.50003L8.186 1.11303ZM15 4.23903L8.5 6.83903V14.761L15 12.161V4.24003V4.23903ZM7.5 14.762V6.83803L1 4.23903V12.162L7.5 14.762V14.762ZM7.443 0.184033C7.80057 0.041032 8.19943 0.041032 8.557 0.184033L15.686 3.03603C15.7787 3.07318 15.8581 3.13723 15.9141 3.21992C15.9701 3.30262 16 3.40018 16 3.50003V12.162C15.9999 12.3619 15.9399 12.5571 15.8278 12.7225C15.7157 12.8879 15.5566 13.0159 15.371 13.09L8.186 15.964C8.06662 16.0119 7.93338 16.0119 7.814 15.964L0.63 13.09C0.444251 13.0161 0.284942 12.8881 0.172642 12.7227C0.0603417 12.5573 0.000206329 12.362 0 12.162L0 3.50003C2.32399e-05 3.40018 0.0299437 3.30262 0.085907 3.21992C0.14187 3.13723 0.221313 3.07318 0.314 3.03603L7.443 0.184033Z" />
@@ -39,12 +40,15 @@
                     </a>
                     <!-- Menu: Sub menu ul -->
                     <ul class="sub-menu collapse show" id="menu_dashboard">
-                        <li><a class="ms-link active" href="">My Dashboard</a></li>
+                        <li><a class="ms-link {{ request()->is('admin/dashboard') ? 'active' : '' }}"
+                                href="{{ route('dashboard') }}">My
+                                Dashboard</a></li>
                     </ul>
                 </li>
                 <!--[ Start:: Account ]-->
                 <li>
-                    <a class="m-link" href="{{ route('user.list') }}">
+                    <a class="m-link {{ request()->is('admin/user-management') ? 'active' : '' }}"
+                        href="{{ route('user.list') }}">
                         <svg width="20" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
                             <path opacity="0.7"
                                 d="M6 8C6.79565 8 7.55871 7.68393 8.12132 7.12132C8.68393 6.55871 9 5.79565 9 5C9 4.20435 8.68393 3.44129 8.12132 2.87868C7.55871 2.31607 6.79565 2 6 2C5.20435 2 4.44129 2.31607 3.87868 2.87868C3.31607 3.44129 3 4.20435 3 5C3 5.79565 3.31607 6.55871 3.87868 7.12132C4.44129 7.68393 5.20435 8 6 8ZM1 14C1 14 0 14 0 13C0 12 1 9 6 9C11 9 12 12 12 13C12 14 11 14 11 14H1Z" />
@@ -60,7 +64,7 @@
                         <span class="mx-3">User Management</span>
                     </a>
                 </li>
-                <li class="collapsed">
+                {{-- <li class="collapsed">
                     <a class="m-link" data-bs-toggle="collapse" data-bs-target="#menu_apps" href="#">
                         <svg width="20" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
                             <path opacity="0.2"
@@ -79,7 +83,7 @@
                         <li><a class="ms-link" href="app-filemanager.html">File Manager</a></li>
                         <li><a class="ms-link" href="app-invoice.html">Invoice</a></li>
                     </ul>
-                </li>
+                </li> --}}
 
             </ul>
 
