d5282c9fc4ef5ce4653ba2640a693034c6406497
Arun Kumar <arunmaurya2500@gmail.com>
2023-07-10 02:31:11 +0530

    added fallback route

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,12 @@ use App\Http\Controllers\AuthenticateController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/login', [AuthenticateController::class, 'login'])->name('login');
-Route::post('/login', [AuthenticateController::class, 'doLogin'])->name('login');
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [AuthenticateController::class, 'login'])->name('login');
+    Route::post('/login', [AuthenticateController::class, 'doLogin'])->name('login');
+});
+
+
+Route::fallback(function () {
+    return redirect()->route('login');
+});
